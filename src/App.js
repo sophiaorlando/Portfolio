355bc9e22439aab584d99d5831d2620664aa6793
sophiaorlando <sophia19orlando@gmail.com>
2021-01-09 14:58:30 -0500

    updated portfolio

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+import React from 'react-bootstrap'
+//components
+import NavBar from "./components/NavBar/NavBar"
+import './App.css'
+
+
+function App() {
+  return (
+    <div>
+
+<NavBar/>
+
+      My portfolio
+    </div>
+
+  );
+}
+
+export default App;
