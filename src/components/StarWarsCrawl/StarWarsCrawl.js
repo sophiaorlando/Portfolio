61b3459131645775cf0663edd4f27050de43770c
sophiaorlando <sophia19orlando@gmail.com>
2021-01-14 14:37:58 -0500

    updated

--- src/components/StarWarsCrawl/StarWarsCrawl.js
+++ src/components/StarWarsCrawl/StarWarsCrawl.js
@@ -16,7 +16,7 @@ function StarWarsCrawl() {
       <h1>Sophia Orlando</h1>
     </div>
 
-    <div className="text-css">An innovate software engineer with emphasis on ReactJS and user accessibility. Take a look at my skills and some
+    <div className="text-css">An innovative software engineer with emphasis on ReactJS and UX/UI design. Take a look at my skills and some
 of my recent projects.</div>
   </div>
 </section>
